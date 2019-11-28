37573c4ac571948f6297c3a99d552f902e351158
AmabileKniss <45179040+AmabileKniss@users.noreply.github.com>
2019-11-28 13:17:22 -0300

    tcc

--- controlador.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "CRUDgerente.php";
-$cod_prod = $_GET['cod_prod'];
-if($_GET['action'] == "excluir"){
-   echo $crud->excluirProduto($cod_prod);
-}else{
-	true;
-}
--- controladorCompra.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "CRUDCompra.php";
-    if($_GET['funcao'] == "cadastro"){
-        cadastro($crud, $nome, $sobrenome, $endereco, $numero, $estado, $cd_cpf, $horarioEntrega, $formaPagamento, $bandeiraCartao, $numeroCartao, $cvc, $mesVencimento, $anoVencimento);
-    }
-    function cadastro($crud, $nome, $sobrenome, $endereco, $numero, $estado, $cd_cpf, $horarioEntrega, $formaPagamento, $bandeiraCartao, $numeroCartao, $cvc, $mesVencimento, $anoVencimento){
-        echo $crud->cadastrarEndereco($_POST['nome'], $_POST['sobrenome'], $_POST['endereco'], $_POST['numero'], $_POST['estado'], $_POST['cd_cpf'], $_POST['horarioEntrega'], $_POST['formaPagamento'], $_POST['bandeiraCartao'], $_POST['numeroCartao'], $_POST['cvc'], $_POST['mesVencimento'], $_POST['anoVencimento']);
-
-    }
